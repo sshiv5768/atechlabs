21cc625c5aaa06f27555b2405131acf6c22aa45b
Shivlal <sshivlal9601@gmail.com>
2021-06-12 13:55:26 +0530

    Day-3 Tasks

---
+++ Day-3/StartEnd.py
@@ -0,0 +1,6 @@
+a=int(input("Enter Starting Number:"))
+b=int(input("Enter Ending Number:"))
+
+# It prints range (a, b)
+for i in range(a,b+1,-3):
+    print("the series is",i)
---
+++ Day-3/SwapNumber.py
@@ -0,0 +1,11 @@
+x = float(input("Enter a number: "))
+y = float(input("Enter a number: "))
+ 
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+ 
+# logic for swap 'x' and 'y'
+x, y = y, x
+ 
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
